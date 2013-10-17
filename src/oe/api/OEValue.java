6a1fc7460c8582146c83a38c86990c8c8993246f
ictrobot <ictrobot@outlook.com>
2013-10-17 19:36:08 +0100

    Made OEValue a wrapper for Values

--- src/oe/api/OEValue.java
+++ src/oe/api/OEValue.java
@@ -1,33 +1,5 @@
 package oe.api;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import oe.value.*;
 
-public class OEValue {
-  
-  public static void setValue(ItemStack itemstack, int value) {
-    Values.add(itemstack, value);
-  }
-  
-  public static int getValue(ItemStack itemstack) {
-    return Values.getValue(itemstack);
-  }
-  
-  public static int getValue(Block block) {
-    return Values.getValue(block);
-  }
-  
-  public static int getValue(Item item) {
-    return Values.getValue(item);
-  }
-  
-  public static String getName() {
-    return Values.name;
-  }
-  
-  public static String getNameFull() {
-    return Values.nameFull;
-  }
-}
+public class OEValue extends Values{ }
