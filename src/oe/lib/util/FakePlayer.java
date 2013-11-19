44df2a98ec5453f8c6e109785a5bc0956a28ea7c
ictrobot <ictrobot@outlook.com>
2013-11-19 19:54:02 +0000

    Update Fake Player, based on Forge's fake player

--- src/oe/lib/util/FakePlayer.java
+++ src/oe/lib/util/FakePlayer.java
@@ -1,12 +1,15 @@
 package oe.lib.util;
 
 import cpw.mods.fml.common.FMLCommonHandler;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemInWorldManager;
+import net.minecraft.network.packet.Packet204ClientInfo;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.stats.StatBase;
 import net.minecraft.util.ChatMessageComponent;
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 public class FakePlayer extends EntityPlayerMP {
@@ -29,6 +32,43 @@ public class FakePlayer extends EntityPlayerMP {
   public void sendChatToPlayer(ChatMessageComponent chatmessagecomponent) {
   }
   
+  public void addStat(StatBase par1StatBase, int par2) {
+  }
+  
+  @Override
+  public void openGui(Object mod, int modGuiId, World world, int x, int y, int z) {
+  }
+  
+  @Override
+  public boolean isEntityInvulnerable() {
+    return true;
+  }
+  
+  @Override
+  public boolean canAttackPlayer(EntityPlayer player) {
+    return false;
+  }
+  
+  @Override
+  public void onDeath(DamageSource source) {
+    return;
+  }
+  
+  @Override
+  public void onUpdate() {
+    return;
+  }
+  
+  @Override
+  public void travelToDimension(int dim) {
+    return;
+  }
+  
+  @Override
+  public void updateClientInfo(Packet204ClientInfo pkt) {
+    return;
+  }
+  
   public static class OEFakePlayer extends FakePlayer {
     
     public OEFakePlayer() {
@@ -37,9 +77,6 @@ public class FakePlayer extends EntityPlayerMP {
     
     public OEFakePlayer(String str) {
       super(MinecraftServer.getServer().worldServers[0], str);
-    }
-    
-    public void addStat(StatBase par1StatBase, int par2) {
     }
   }
 }
