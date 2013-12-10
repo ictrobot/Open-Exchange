b3c4fcedf50d0b317c0f1dc441b52eb3b37960ff
ictrobot <ictrobot@outlook.com>
2013-12-10 21:19:20 +0000

    Improvements to Tooltips & TileInfo

--- src/oe/core/handler/ToolTipHandler.java
+++ src/oe/core/handler/ToolTipHandler.java
@@ -1,5 +1,6 @@
 package oe.core.handler;
 
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import oe.api.OEItemInterface;
@@ -22,6 +23,10 @@ public class ToolTipHandler {
       double stored = oe.getQMC(event.itemStack);
       if (oe != null && stored >= 0) {
         event.toolTip.add("Stored: " + QMC.formatter.format(oe.getQMC(event.itemStack)) + " " + QMC.name);
+      }
+      if (GuiScreen.isShiftKeyDown()) {
+        event.toolTip.add("Max Storable: " + QMC.formatter.format(oe.getMaxQMC(event.itemStack)) + " " + QMC.name);
+        event.toolTip.add("Tier: " + oe.getTier(event.itemStack));
       }
     }
     if (QMC.hasQMC(event.itemStack)) {
--- src/oe/network/packet/TileInfoPacket.java
+++ src/oe/network/packet/TileInfoPacket.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraft.tileentity.TileEntity;
@@ -36,10 +37,20 @@ public class TileInfoPacket {
       if (te != null) {
         if (OE.isOE(te.getClass())) {
           OETileInterface oe = (OETileInterface) te;
-          player.sendChatToPlayer(ChatMessageComponent.createFromText("\u00A73\u00A7l" + QMC.name + " Info:\u00A7r\u00A77 " + oe.getQMC() + " " + QMC.name + ", " + oe.getMaxQMC() + " Max " + QMC.name + ", Type: " + oe.getType()));
+          player.sendChatToPlayer(ChatMessageComponent.createFromText("\u00A73\u00A7l" + QMC.name + " Info:\u00A7r\u00A77 " + QMC.name + ": " + oe.getQMC() + ", Max " + QMC.name + ": " + oe.getMaxQMC() + ", Tier: " + oe.getTier() + ", Type: " + oe.getType()));
         } else if (InWorldQMC.isOEPipe(te.getClass())) {
           OEPipeInterface oe = (OEPipeInterface) te;
-          player.sendChatToPlayer(ChatMessageComponent.createFromText("\u00A73\u00A7l" + QMC.name + " Info:\u00A7r\u00A77 " + (oe.getMaxQMC() - oe.passThroughLeft()) + " " + QMC.name + ", " + oe.getMaxQMC() + " Max " + QMC.name));
+          player.sendChatToPlayer(ChatMessageComponent.createFromText("\u00A73\u00A7l" + QMC.name + " Info:\u00A7r\u00A77 " + QMC.name + ": " + (oe.getMaxQMC() - oe.passThroughLeft()) + ", Max " + QMC.name + ": " + oe.getMaxQMC()));
+        } else {
+          ItemStack stack = new ItemStack(player.worldObj.getBlockId(x, y, z), 1, player.worldObj.getBlockMetadata(x, y, z));
+          if (QMC.hasQMC(stack)) {
+            player.sendChatToPlayer(ChatMessageComponent.createFromText("\u00A73\u00A7l" + QMC.name + " Info:\u00A7r\u00A77 " + QMC.name + ": " + QMC.getQMC(stack)));
+          }
+        }
+      } else {
+        ItemStack stack = new ItemStack(player.worldObj.getBlockId(x, y, z), 1, player.worldObj.getBlockMetadata(x, y, z));
+        if (QMC.hasQMC(stack)) {
+          player.sendChatToPlayer(ChatMessageComponent.createFromText("\u00A73\u00A7l" + QMC.name + " Info:\u00A7r\u00A77 " + QMC.name + ": " + QMC.getQMC(stack)));
         }
       }
     }
